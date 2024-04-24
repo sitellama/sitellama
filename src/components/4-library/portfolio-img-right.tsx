5c6cbf5b522a5e18718806adfe4dd06a06119942
Karina Rogers <krogers@ucdavis.edu>
2024-04-23 20:20:23 -0400

    feat: begin portfolio section

---
+++ src/components/4-library/portfolio-img-right.tsx
@@ -0,0 +1,26 @@
+
+
+export function PortfolioImgRight({
+    projectImgSrc,
+    projectImgAlt,
+    projectName,
+    projectQuickFacts,
+    projectDesc }: {
+        projectImgSrc: string;
+        projectImgAlt: string;
+        projectName: string;
+        projectQuickFacts: string;
+        projectDesc: string;
+    }) {
+    return (
+        <div className="flex flex-col items-center w-screen sm:flex-row bg-gray-400">
+            <div className="w-1/2 p-5 bg-gray-400" >
+                <h3>{projectName}</h3>
+                <p className="uppercase">{projectQuickFacts}</p>
+                <p>{projectDesc}</p>
+                <a className="inline-block bg-purple-500 p-4 rounded-md">Visit Live Site</a>
+            </div>
+            <img src={projectImgSrc} alt={projectImgAlt} className="w-1/2 max-h-[500px] object-cover" />
+        </div>
+    );
+}
