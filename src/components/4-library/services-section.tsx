a4c8b2e8abe10e7940433ba1991f4d661b86a162
Tanner Cook <tannercook336@gmail.com>
2024-04-23 22:52:47 -0400

    chore: css styling on Hero and Pricing

--- src/components/4-library/services-section.tsx
+++ src/components/4-library/services-section.tsx
@@ -7,41 +7,41 @@ function ServicesSection() {
         description: "Lorem ipsum dolor sit amet, #brands."
     },
     {
-        imageUrl: "src/assets/lightning-bolt.svg",
+        imageUrl: "src/assets/smileyface.svg",
         title: "Hosting",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     },
     {
-        imageUrl: "src/assets/lightbulb.png",
+        imageUrl: "src/assets/smileyface.svg",
         title: "Web Design",
         description: "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     },
     {
-        imageUrl: "src/assets/idea.png",
+        imageUrl: "src/assets/smileyface.svg",
         title: "Web Development",
-        description: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        description: "Ut enim ad minim veniam, quis nostrud exercitation"
     },
     {
-        imageUrl: "src/assets/gears.png",
+        imageUrl: "src/assets/smileyface.svg",
         title: "Maintenence",
-        description: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        description: "Ut enim ad minim veniam, quis nostrud exercitation"
     },
     {
         imageUrl: "src/assets/smileyface.svg",
         title: "Site Revamp",
-        description: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        description: "Ut enim ad minim veniam, quis nostrud exercitation"
     },
       
   ];
 
   // ServiceCard component
   const ServiceCard = ({ imageUrl, title, description }) => (
-    <a href="#" className="shadow-2xl relative rounded-lg">
-      <div className="h-full relative shadow-2x1 shadow-purple-900 overflow-hidden group">
-        <div className="absolute -bottom-10 group-hover:top-0 left-0 w-full h-full group-hover:bg-purple-900 transition-all ease-in-out duration-500">
+    <a href="#" className="shadow-3xl relative bg-black rounded-3xl">
+      <div className="h-full relative shadow-2x1 shadow-purple-900 overflow-hidden group rounded-3xl bg-red-500">
+        <div className="absolute group-hover:top-0 left-0 w-full h-full group-hover:bg-green-900 trasition-opacity opacity-0 hover:opacity-100 duration-200">
           <div className="w-3/6 h-full p-3 relative">
             <div className="absolute bottom-0 group-hover:bottom-24 text-white text-left transition-all ease-in-out duration-500">
-              <h2 className="text-2xl font-bold text-white mb-0 pb-1">{title}</h2>
+              <h2 className="text-2xl font-bold text-amber-600 mb-0 pb-1">{title}</h2>
               <p className="text-lg font-light text-white">{description}</p>
             </div>
           </div>
@@ -53,7 +53,7 @@ function ServicesSection() {
 
   return (
     <section className="sm:py-12 lg:py-16 z-40 relative">
-      <div className="container mx-auto">
+      <div className="container w-[1000px] mx-auto">
         <h2 className="text-3xl font-light text-black sm:text-4xl lg:text-5xl">
           What can{' '}
           <span className="block w-full font-light text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-purple-500 lg:inline">
@@ -65,7 +65,7 @@ function ServicesSection() {
           We make your ideas with a whole Llama love!
         </p>
 
-        <div className="grid grid-cols-1 gap-6 lg:grid-cols-3 rounded">
+        <div className="grid grid-cols-1 gap-10 lg:grid-cols-3 rounded">
           {services.map((service, index) => (
             <ServiceCard
               key={index}
