1cc91bc06e799b5000235cbc71dc5b4dbb020e9e
Karina Rogers <krogers@ucdavis.edu>
2024-06-29 01:09:34 -0400

    chore: slider colors/imgs

--- src/components/4-library/carousel.tsx
+++ src/components/4-library/carousel.tsx
@@ -32,7 +32,11 @@ export default function Carousel() {
         },
         {
             name: "3",
-            img: "src/assets/example1.png",
+            img: "src/assets/example3.png",
+        },
+        {
+            name: "3",
+            img: "src/assets/example4.png",
         },
     ];
 
@@ -44,7 +48,7 @@ export default function Carousel() {
         slidesToScroll: 1,
         centerMode: true,
         // centerPadding: "0px",
-        className: "bg-red-500",
+        // className: "bg-red-500",
         // autoplay: true,
         // autoplaySpeed: 2000,
         variableWidth: true,
@@ -83,16 +87,17 @@ export default function Carousel() {
                         sliderRef = slider;
                     }}>
                         {data.map((item, index) => (
-                            <div className="bg-white text-green-400 rounded-xl" key={index}>
-                                <div className="rounded-t-xl bg-indigo-500 flex justify-center items-center">
-                                    <img className="size-48" src={item.img} alt={item.name} />
+                            <div className=" text-green-400 rounded-xl" key={index}>
+                                <div className="rounded-t-xl flex justify-center items-center">
+                                    <img className="size-36" src={item.img} alt={item.name} />
                                 </div>
                             </div>
                         ))}
                     </Slider>
                 </div>
             </div>
-            <div className="flex justify-center md:justify-end gap-6 mt-4 md:mr-4">
+
+            <div className="flex justify-center md:justify-end gap-12 mt-4 md:mr-4">
                 <button className="bg-brand-purple rounded-full p-2" onClick={previous}>
                     <ArrowLeft className="size-8 stroke-white" />
                     <span className="sr-only">
