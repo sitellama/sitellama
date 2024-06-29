3264cd4d40c4c4cbac2e69819ee732d76935ea2a
maxzakh <maks.zakh@gmail.com>
2024-06-29 01:17:36 -0400

    fix:remove unused component

--- src/components/2-main/about/about.tsx
+++ src/components/2-main/about/about.tsx
@@ -1,12 +1,12 @@
 import { SEO } from "@/utils/seo";
-import { WorkWithUs } from "../../4-library/work-with-us";
+// import { WorkWithUs } from "../../4-library/work-with-us";
 
 export function About() {
     return (
         <>
             <SEO title="About" description="This is the about page" href="/" />
 
-            <WorkWithUs />
+            {/* <WorkWithUs /> */}
         </>
     );
 }
